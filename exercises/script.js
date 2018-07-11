a0b716f8fe27501ae608397faa8f01de247e5434
Qiuzo <qiuzoace@gmail.com>
2018-07-11 17:06:53 -0400

    nice

--- exercises/script.js
+++ exercises/script.js
@@ -1,13 +1,13 @@
 document.querySelector('.puzzle-0').addEventListener('click', function() {
-	  window.alert('Hello, World!');
+	  alert('Hello, World!');
 	});
 
 	document.querySelector('.puzzle-1').addEventListener('click', function() {
-		
+		alert('Bonjour, Monde!')
 	});
 
 	document.querySelector('.puzzle-2').addEventListener('click', function() {
-
+		document.querySelector('.gggg').textContent = "no i dont wanna"
 	});
 
 	document.querySelector('.puzzle-3').addEventListener('click', function() {
@@ -15,36 +15,38 @@ document.querySelector('.puzzle-0').addEventListener('click', function() {
 	});
 
 	document.querySelector('.puzzle-4').addEventListener('click', function() {
-
+		document.querySelector('img').style.display = "none"
 	});
 
 	document.querySelector('.puzzle-5').addEventListener('click', function() {
-
+			document.querySelector('img').style.display = "block"
 	});
 
 	document.querySelector('.puzzle-6-yellow').addEventListener('click', function() {
-		
+		document.querySelector('body').style.background = 'yellow'
 	});
 
 	document.querySelector('.puzzle-6-pink').addEventListener('click', function() {
-		
+			document.querySelector('body').style.background = 'pink'
 	});
 
 	document.querySelector('.puzzle-6-purple').addEventListener('click', function() {
-		
+			document.querySelector('body').style.background = 'purple'
 	});
 
 	document.querySelector('.puzzle-7').addEventListener('click', function() {
-		
+		document.querySelector('body').style.fontFamily = ('Comic Sans MS')
+		document.querySelector('body').style.color = ('red')
 	});
 
 	document.querySelector('.puzzle-8').addEventListener('click', function() {
+		document.querySelector('a').href = "https://www.youtube.com/watch?v=gLZ264vO1E0";
+		document.querySelector('a').textContent = "big oof";
+		})
+	document.querySelector('.puzzle-9').addEventListener('click', function() {
+		document.querySelector('body').style.fontFamily = ('Monaco')
+	})
 
-	});
-
-	// Challenge puzzles
-
-	document.querySelector('.puzzle-9')
 
 	document.querySelector('.puzzle-10')
 
@@ -52,8 +54,9 @@ document.querySelector('.puzzle-0').addEventListener('click', function() {
 
 	document.querySelector('.puzzle-12')
 
-	document.querySelector('.puzzle-13')
+	document.querySelector('.puzzle-0')
 
-	document.querySelector('.puzzle-14')
-
+	document.querySelector('.puzzle-13').addEventListener('click', function() {
+		document.querySelector('.reee').innerHTML = '<marquee>I wish I was marquee...</marquee>';
+		})
 	document.querySelector('.puzzle-15')
